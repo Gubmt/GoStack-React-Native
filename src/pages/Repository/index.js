34e09eebcec79261cf53eaad697eee732d483ee2
Gustavo Barbieri <gustavobarbieri@ymail.com>
2019-10-25 17:48:45 -0300

    chore: Iniciando com React-Native

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import PropTypes from 'prop-types';
+
+export default class Repository extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('stars').html_url,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const stars = navigation.getParam('stars');
+
+    return <WebView source={{ uri: stars.html_url }} style={{ flex: 1 }} />;
+  }
+}
